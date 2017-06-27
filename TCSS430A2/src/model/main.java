060318e3ff9387d3554cde4d0a143405a6541b0e
Tommy Warren <taw91@uw.edu>
2017-06-27 12:38:02 -0700

    ArrayList implementation and created a way to remove a router from the list.

--- TCSS430A2/src/model/main.java
+++ TCSS430A2/src/model/main.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * 
  * @author Tommy Warren
@@ -8,6 +11,13 @@ package model;
 public class main {
 
 	public static void main(String[] args) {
+		
+		ArrayList<Router> routers = new ArrayList<Router>();
+		Random rand = new Random(System.currentTimeMillis());
+		
+		
+		
+		
 		Router router1 = new Router();
 		Router router2 = new Router();
 		Router router3 = new Router();
@@ -15,10 +25,22 @@ public class main {
 		
 		router1.addNode("e1", "192.168.1.0");
 		router1.addNode("e2", router2);
+		
 		router2.addNode("e1", router1);
-		router3.addNode("e2", router2);
-		router1.addNode("e1", "192.168.1.0");
-		router1.addNode("e2", router2);
+		
+		router3.addNode("e1", router2);
+		router3.addNode("e2", router1);
+		
+		router3.addNode("e3", router4);
+		
+		routers.add(router1);
+		routers.add(router2);
+		routers.add(router3);
+		routers.add(router4);
+		
+		
+		routers.remove(rand.nextInt() * 0 + 3);
+		
 
 	}
 
