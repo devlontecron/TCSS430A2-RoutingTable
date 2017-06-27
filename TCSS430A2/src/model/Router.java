a03158304d6802fe749ef5b075d9d88e32550d2e
Devin Durham <devindurham95@gmail.com>
2017-06-27 11:42:46 -0700

    router setter and getters

--- TCSS430A2/src/model/Router.java
+++ TCSS430A2/src/model/Router.java
@@ -1,12 +1,60 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * 
- * @author Tommy Warren
+ * @author Tommy Warren, Devin Durham, 
  * @version 6/27/2017
  *
  */
 
 public class Router {
 	
+	private static ArrayList<String> ipArr = new ArrayList<String>();
+	private static ArrayList<Integer> hopsArr = new ArrayList<Integer>();
+	private static ArrayList<Integer> portsArr = new ArrayList<Integer>();
+	private static ArrayList<Date> timeArr = new ArrayList<Date>();
+	
+	public static void Router(){
+		
+		
+		
+	}
+
+	public static ArrayList<String> getIpArr() {
+		return ipArr;
+	}
+
+	public static void setIpArr(String ip) {
+		ipArr.add(ip);
+	}
+
+	public static ArrayList<Integer> getHopsArr() {
+		return hopsArr;
+	}
+
+	public static void setHopsArr(int hops) {
+		hopsArr.add(hops);
+	}
+
+	public static ArrayList<Integer> getPortsArr() {
+		return portsArr;
+	}
+
+	public static void setPortsArr(int ports) {
+		portsArr.add(ports);
+	}
+
+	public static ArrayList<Date> getTimeArr() {
+		return timeArr;
+	}
+
+	public static void setTimeArr(Date time) {
+		timeArr.add(time);
+	}
+	
+		
+
 }
