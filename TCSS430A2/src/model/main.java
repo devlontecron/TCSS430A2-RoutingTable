606d61fd1248d572330fa11c70a67797ea7d3add
Tommy Warren <taw91@uw.edu>
2017-06-29 10:22:58 -0700

    Updated main to add more connections.

--- TCSS430A2/src/model/main.java
+++ TCSS430A2/src/model/main.java
@@ -16,8 +16,6 @@ public class main {
 		Random rand = new Random(System.currentTimeMillis());
 		
 		
-		
-		
 		Router router1 = new Router();
 		Router router2 = new Router();
 		Router router3 = new Router();
@@ -25,23 +23,39 @@ public class main {
 		
 		router1.addNode("e1", "192.168.1.0");
 		router1.addNode("e2", router2);
+		router1.addNote("e3", router3);
 		
 		router2.addNode("e1", router1);
+		router2.addNode("e2", "192.168.2.0");
+		router2.addNode("e3", router3);
 		
 		router3.addNode("e1", router2);
 		router3.addNode("e2", router1);
-		
 		router3.addNode("e3", router4);
+		
+		router4.addNode("e1", "192.168.3.0");
+		router4.addNode("e2", "192.168.4.0");
+		router4.addNode("e3", "192.168.5.0");
 		
 		routers.add(router1);
 		routers.add(router2);
 		routers.add(router3);
 		routers.add(router4);
 		
+		for(Router router : routers){
+			router.displayTable();
+		}
 		
-		routers.remove(rand.nextInt() * 0 + 3);
+		// Remove a random router from the network.
+		routers.remove(rand.nextInt() * 0 + routers.size() - 1);
 		
-
+		
+		for(Router router : routers){
+			router.updateTable();
+			router.displayTable();
+		}
+		
+		
 	}
 
 }
