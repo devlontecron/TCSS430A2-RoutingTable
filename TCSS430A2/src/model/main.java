70da7644cf8e55e7763b55505071fa9ba982966e
Devin Durham <devindurham95@gmail.com>
2017-07-01 00:43:10 -0700

    formatting, tweaking

--- TCSS430A2/src/model/main.java
+++ TCSS430A2/src/model/main.java
@@ -6,14 +6,14 @@ import java.util.Random;
 
 /**
  * 
- * @author Tommy Warren
+ * @author Tommy Warren, Devin Durham
  *	@version 6/27/2017
  */
 public class main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Router> routers = new ArrayList<Router>();
+		ArrayList<Router> routersArray = new ArrayList<Router>();
 		Random rand = new Random(System.currentTimeMillis());
 		int chosenRouter;
 		Date timeStamp = new Date(System.currentTimeMillis());
@@ -24,36 +24,36 @@ public class main {
 		Router router4 = new Router();
 		
 		
-		
+		//Adding each routers loop-back to their table
 		router1.addTableEntry("192.168.1.0", 0, null, timeStamp.toString());
 		router2.addTableEntry("192.168.2.0", 0, null, timeStamp.toString());
 		router3.addTableEntry("192.168.3.0", 0, null, timeStamp.toString());
 		router4.addTableEntry("192.168.4.0", 0, null, timeStamp.toString());
 		
+		//Adding entries for each routers tables
+		router1.addTableEntry(router2.getIPFromIndex(0),1, "a1", timeStamp.toString());
+		router1.addTableEntry(router3.getIPFromIndex(0),1, "a2", timeStamp.toString());
 		
-		router1.addTableEntry(router2.getIPFromIndex(0), router2.getHopsFromIndex(0) + 1, "e1", timeStamp.toString());
-		router1.addTableEntry("192.168.1.1", 1, "e2", timeStamp.toString());
-		router1.addTableEntry(router3.getIPFromIndex(0), router3.getHopsFromIndex(0) + 1, "e3", timeStamp.toString());
+		router2.addTableEntry(router1.getIPFromIndex(0),1, "b1", timeStamp.toString());
+		router2.addTableEntry(router3.getIPFromIndex(0),1, "b2", timeStamp.toString());
+		
+		router3.addTableEntry(router1.getIPFromIndex(0),1, "c1", timeStamp.toString());
+		router3.addTableEntry(router2.getIPFromIndex(0),1, "c2", timeStamp.toString());
+		router3.addTableEntry(router4.getIPFromIndex(0),1, "c3", timeStamp.toString());
+		
+		router4.addTableEntry(router3.getIPFromIndex(0),1, "d1", timeStamp.toString());
+		
+		//adding routers to list of routers
+		routersArray.add(router1);
+		routersArray.add(router2);
+		routersArray.add(router3);
+		routersArray.add(router4);
+		
+		//randomly choosing a router to remove
+		chosenRouter = rand.nextInt() * 0 + routersArray.size() - 1;
 		
 		
-		router2.addTableEntry(router1.getIPFromIndex(0), router1.getHopsFromIndex(0) + 1, "e1", timeStamp.toString());
-		router2.addTableEntry("192.168.2.1", 1, "e2", timeStamp.toString());
-		router2.addTableEntry(router3.getIPFromIndex(0), router3.getHopsFromIndex(0) + 1, "e3", timeStamp.toString());
-		
-		router3.addTableEntry(router1.getIPFromIndex(0), router1.getHopsFromIndex(0) + 1, "e1", timeStamp.toString());
-		router3.addTableEntry(router2.getIPFromIndex(0), router2.getHopsFromIndex(0) + 1, "e2", timeStamp.toString());
-		router3.addTableEntry(router4.getIPFromIndex(0), router4.getHopsFromIndex(0) + 1, "e3", timeStamp.toString());
-		
-		router4.addTableEntry(router3.getIPFromIndex(0), router3.getHopsFromIndex(0) + 1, "e1", timeStamp.toString());
-		
-		routers.add(router1);
-		routers.add(router2);
-		routers.add(router3);
-		routers.add(router4);
-		
-		chosenRouter = rand.nextInt() * 0 + routers.size() - 1;
-		
-		for(Router router : routers){
+		for(Router router : routersArray){
 			router.displayTable();
 		}
 		
@@ -76,9 +76,11 @@ public class main {
 				break;
 		
 		}
-		routers.remove(chosenRouter);
+		routersArray.remove(chosenRouter);
 		
-		for(Router router : routers){
+		System.out.println("\nRouter " + (chosenRouter+1) +" has failed");
+		
+		for(Router router : routersArray){
 			router.displayTable();
 		}
 	}
