3febb3579eb1319e031f624f260de5e3e5be1b44
Devin Durham <devindurham95@gmail.com>
2017-07-01 00:43:53 -0700

    fixed class to non static instances and private data structures

--- TCSS430A2/src/model/Router.java
+++ TCSS430A2/src/model/Router.java
@@ -11,115 +11,109 @@ import java.util.Date;
  */
 
 public class Router {
-	private static Router rt = new Router();
 
-	private static ArrayList<String> ipArr = new ArrayList<String>();
-	private static ArrayList<Integer> hopsArr = new ArrayList<Integer>();
-	private static ArrayList<String> portsArr = new ArrayList<String>();
-	private static ArrayList<String> timeArr = new ArrayList<String>();
-	public static ArrayList<Router> routerArr = new ArrayList<Router>();
+	private ArrayList<String> ipArr = new ArrayList<String>();
+	private ArrayList<Integer> hopsArr = new ArrayList<Integer>();
+	private ArrayList<String> portsArr = new ArrayList<String>();
+	private ArrayList<String> timeArr = new ArrayList<String>();
+	private ArrayList<Router> routerArr = new ArrayList<Router>();
 
-	private static Date myTime = new Date();
+	private Date myTime = new Date();
 
-	public static void Router() {
-
-		addTableEntry("192", 0, "PORT", myTime.toString());
-
+	public String getIPFromIndex(int x) {
+		return this.ipArr.get(x);
 	}
 
-	public static String getIPFromIndex(int x) {
-		return ipArr.get(x);
+	public int getHopsFromIndex(int x) {
+		return this.hopsArr.get(x);
 	}
 
-	public static int getHopsFromIndex(int x) {
-		return hopsArr.get(x);
+	public String getPortFromIndex(int x) {
+		return this.portsArr.get(x);
 	}
 
-	public static String getPortFromIndex(int x) {
-		return portsArr.get(x);
+	public String getTimeFromIndex(int x) {
+		return this.timeArr.get(x);
 	}
 
-	public static String getTimeFromIndex(int x) {
-		return timeArr.get(x);
+	public Router getRouterFromIndex(int x) {
+		return this.routerArr.get(x);
 	}
 
-	public static Router getRouterFromIndex(int x) {
-		return routerArr.get(x);
+	public void setRouterArr(Router route) {
+		this.routerArr.add(route);
 	}
 
-	public static void setRouterArr(Router route) {
-		routerArr.add(route);
+	public ArrayList<Router> getRouterArr() {
+		return this.routerArr;
 	}
 
-	public static ArrayList<Router> getRouterArr() {
-		return routerArr;
+	public ArrayList<String> getIpArr() {
+		return this.ipArr;
 	}
 
-	public static ArrayList<String> getIpArr() {
-		return ipArr;
+	public void setIpArr(String ip) {
+		this.ipArr.add(ip);
 	}
 
-	public static void setIpArr(String ip) {
-		ipArr.add(ip);
+	public ArrayList<Integer> getHopsArr() {
+		return this.hopsArr;
 	}
 
-	public static ArrayList<Integer> getHopsArr() {
-		return hopsArr;
+	public void setHopsArr(int hops) {
+		this.hopsArr.add(hops);
 	}
 
-	public static void setHopsArr(int hops) {
-		hopsArr.add(hops);
+	public ArrayList<String> getPortsArr() {
+		return this.portsArr;
 	}
 
-	public static ArrayList<String> getPortsArr() {
-		return portsArr;
+	public void setPortsArr(String ports) {
+		this.portsArr.add(ports);
 	}
 
-	public static void setPortsArr(String ports) {
-		portsArr.add(ports);
+	public ArrayList<String> getTimeArr() {
+		return this.timeArr;
 	}
 
-	public static ArrayList<String> getTimeArr() {
-		return timeArr;
+	public void setTimeArr(Date time) {
+		this.timeArr.add(time.toString());
 	}
 
-	public static void setTimeArr(Date time) {
-		timeArr.add(time.toString());
+	public void removeIP(int x) {
+		this.ipArr.remove(x);
 	}
 
-	public static void removeIP(int x) {
-		ipArr.remove(x);
+	public void removeHop(int x) {
+		this.hopsArr.remove(x);
 	}
 
-	public static void removeHop(int x) {
-		hopsArr.remove(x);
+	public void removePorts(int x) {
+		this.portsArr.remove(x);
 	}
 
-	public static void removePorts(int x) {
-		portsArr.remove(x);
+	public void removeTime(int x) {
+		this.timeArr.remove(x);
 	}
 
-	public static void removeTime(int x) {
-		timeArr.remove(x);
+	public void removeTableEntryAtIndex(int x) {
+		this.timeArr.remove(x);
+		this.portsArr.remove(x);
+		this.hopsArr.remove(x);
+		this.ipArr.remove(x);
 	}
 
-	public static void removeTableEntryAtIndex(int x) {
-		timeArr.remove(x);
-		portsArr.remove(x);
-		hopsArr.remove(x);
-		ipArr.remove(x);
-	}
-
-	public static void addTableEntry(String ip, int hops, String ports, String time) {
-		timeArr.add(time);
-		portsArr.add(ports);
-		hopsArr.add(hops);
-		ipArr.add(ip);
+	public void addTableEntry(String ip, int hops, String ports, String time) {
+		this.timeArr.add(time);
+		this.portsArr.add(ports);
+		this.hopsArr.add(hops);
+		this.ipArr.add(ip);
 	}
 
 	public void displayTable() {
-		System.out.println("Port   		Ip Adrress   Hops   	Time");
-		for (int i = 0; i < 10; i++) {
+		
+		System.out.println("\nPort   		Ip Adrress   Hops   	Time");
+		for (int i = 0; i < ipArr.size(); i++) {
 			System.out.printf("%-15s%-17s%-5s%1s", portsArr.get(i), ipArr.get(i),hopsArr.get(i),timeArr.get(i));
 			System.out.println("");
 			
